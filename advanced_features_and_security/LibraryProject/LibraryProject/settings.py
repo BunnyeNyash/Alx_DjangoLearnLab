fc53f10439ede49e6f73139b7595e04a5dc8cdde
BunnyeNyash <125456472+BunnyeNyash@users.noreply.github.com>
2025-07-27 23:22:09 +0300

    use custom user model

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -127,6 +127,8 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 LOGIN_REDIRECT_URL = '/relationship_app/books/'
 LOGOUT_REDIRECT_URL = '/relationship_app/login/'
 
+AUTH_USER_MODEL = 'relationship_app.CustomUser'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
