2bf86a9d5abd247875732668cea046452595d160
BunnyeNyash <125456472+BunnyeNyash@users.noreply.github.com>
2025-07-27 23:37:04 +0300

    Update views.py

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import permission_required
+from django.shortcuts import redirect
+from .models import Book
 
 # Create your views here.
