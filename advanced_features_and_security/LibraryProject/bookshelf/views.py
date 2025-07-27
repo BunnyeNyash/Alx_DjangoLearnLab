bc8915f57caf045f58550a39e9136164df22c528
BunnyeNyash <125456472+BunnyeNyash@users.noreply.github.com>
2025-07-27 23:44:02 +0300

    Update views.py

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import permission_required
 from django.shortcuts import redirect
-from .models import Book
+from .models import Book, Author
+from django import forms
 
 # Create your views here.
