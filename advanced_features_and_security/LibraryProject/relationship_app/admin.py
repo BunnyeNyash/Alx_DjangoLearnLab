8ced0dd1de2e0e7375fd955842c4e5487fce3c72
BunnyeNyash <125456472+BunnyeNyash@users.noreply.github.com>
2025-07-27 23:30:01 +0300

    Update admin.py

--- advanced_features_and_security/LibraryProject/relationship_app/admin.py
+++ advanced_features_and_security/LibraryProject/relationship_app/admin.py
@@ -3,11 +3,3 @@ from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
 
 # Register your models here.
-class CustomUserAdmin(UserAdmin):
-    model = CustomUser
-    list_display = ['username', 'email', 'date_of_birth', 'is_staff']
-    fieldsets = UserAdmin.fieldsets + (
-        (None, {'fields': ('date_of_birth', 'profile_photo')}),
-    )
-
-admin.site.register(CustomUser, CustomUserAdmin)
