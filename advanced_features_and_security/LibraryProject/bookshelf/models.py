c52b32c2b79658efb886025101a4c64df9d1fb77
BunnyeNyash <125456472+BunnyeNyash@users.noreply.github.com>
2025-07-27 23:33:32 +0300

    Update models.py

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -6,9 +6,17 @@ from django.conf import settings
 # Create your models here.
 class Book(models.Model):
     title = models.CharField(max_length=200)
-    author = models.CharField(max_length=100)
+    # author = models.CharField(max_length=100)
+    author = models.ForeignKey('Author', on_delete=models.CASCADE)
     publication_year = models.IntegerField()
 
+    class Meta:
+        permissions = [
+            ('can_view', 'Can view book'),
+            ('can_create', 'Can create book'),
+            ('can_edit', 'Can edit book'),
+            ('can_delete', 'Can delete book'),
+        ]
 
 class CustomUser(AbstractUser):
     date_of_birth = models.DateField(null=True, blank=True)
